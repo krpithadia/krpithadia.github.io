f60178f110686869bb633047f87d555999cf65e9
Kaushal <krpithadia@gmail.com>
2025-08-18 23:31:16 +0530

    sitekey restructured

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  RECAPTCHA_SITE_KEY: '6LccSaYrAAAAAFWwUciXJzFysDOXKB3YdyaUYTlC',
+  RECAPTCHA_SITE_KEY: '6LevbaorAAAAANj6LiwRetF77FUTUFfpnNj638_C',
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzqO2ja2u8moARFehf7J1fi6YPHbvz_BfmuAa_qVnAn9DTUMmzFgPAmQGwJKEyUFZWl/exec'
 };
